cd04509a71d4aa09bd336f5076f98c748839c4e5
Felicia Phillips  🌱 <feliciamp@proton.me>
2025-08-27 22:05:55 -0500

    Homepage update

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -10,6 +10,7 @@ import Layout from "../../components/Layout/Layout.jsx";
 //Images
 
 import citysilo from '../../assets/citysilo.jpg';
+import veganDish from '../../assets/veganDish.webp';
 
 const Home =() => {
 return(
@@ -32,11 +33,31 @@ return(
 </section>
 
 <div className={styles.reccomendations}>
-  <h4>Best Vegan Bakery's in Memphis:</h4>
+  <h4>Vegan Bakery's in Memphis:</h4>
 <div className={styles.reccomendationsRestaurant}>
   <Article description={"The Pink Bakery"}/>
   <Article description={"Muddy's Bake Shop"}/>
   <Article description={"Frost Bake Shop"}/>
+  </div>
+</div>
+
+<div className={styles.newsletter}>
+  <div className={styles.newsletterLeft}>
+    
+  </div>
+  <div className={styles.newsletterRight}>
+    <form className={styles.newsletterForm}>
+      <h2 className={styles.newsletterTitle}>Subscribe to our Newsletter</h2>
+      <input
+        type="email"
+        placeholder="Enter your email"
+        required
+        className={styles.emailInput}
+      />
+      <button type="submit" className={styles.subscribeButton}>
+        Subscribe
+      </button>
+    </form>
   </div>
 </div>
 
